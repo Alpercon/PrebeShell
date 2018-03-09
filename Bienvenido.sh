771d21ab5a5da2911f23b5a71abacd4aed454c85
Armando101 <35951139+Armando101@users.noreply.github.com>
2018-03-09 02:27:42 -0600

    Modificaciones y disenio

--- Bienvenido.sh
+++ Bienvenido.sh
@@ -1,13 +1,16 @@
 #!/bin/bash
 clear
-
-echo -e "Usted se ha logeado coorrectamente\n"
-echo -e "\nHola Bienvenido a la PrebeShell"
-while [[ true ]]; do
-	echo "Introduce manpage para ver los comandos que se pueden utilizar y una descripcion de ellos"
-	echo "Introduce 'salir' para salir de la Prebeshell"
-	echo ""
-	read -p "$USER"@"$HOSTNAME":"$PWD" var		##Promt
+BLUE='\033[1;34m'
+GREEN='\033[1;32m'
+WHITE='\033[1;37m'
+figlet -c "Bienvenido a la PrebeShell"
+echo "Introduce manpage para ver los comandos que se pueden utilizar y una descripcion de ellos"
+echo "Introduce 'salir' para salir de la Prebeshell"
+echo ""
+var =""
+while [[ "$var" != "salir" ]]; do
+	echo -ne "${GREEN}$USER"@"$HOSTNAME":"${BLUE}$PWD$ ${WHITE}"
+	read -e var
 	if [[ "$var" == "manpage" ]]; then
 		 echo "Aun no existe este script" #bash $PWD/manpage.sh
 		 exit 0;
@@ -15,6 +18,6 @@ while [[ true ]]; do
 		echo -e "\nVuelve Pronto"
 		exit 0;
 	else
-		echo -e "Opcion no valida\nSeleccione otra opcion" 
+		$var
 	fi
 done
