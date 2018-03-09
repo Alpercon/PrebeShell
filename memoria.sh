1824aae8669ea2c9cf772ebac978252e6e3ed304
Armando <rivera.armando997@gmail.com>
2018-03-09 04:40:12 -0600

    Disenio del juego

--- memoria.sh
+++ memoria.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
 clear
 
-echo "Bienvenido al juego de memoria"
+figlet -c "Bienvenido al juego de memoria"
 echo "Instrucciones:"
 echo "Se mostraran numeros al azar despues tu tendras que poner el conjunto de numeros mostrados"
+
+BLUE='\033[1;34m'
+RED='\033[1;31m'
+GREEN='\033[1;32m'
 
 numeros1=(3 54)
 numeros2=(56 78 7)
@@ -15,13 +19,14 @@ clear
 for i in ${numeros1[@]}
 do
 	clear
-	echo $i
+	figlet -c $i
 	sleep 2
 	clear
 done
 read -p "> " n
 if [[ "$n" == "3 54" ]]; then
-	echo "pasaste al nivel 2"
+	echo -e "${GREEN}pasaste al nivel 2"
+	sleep 2
 	else
 		echo "Lo sentimos has perdido"
 		echo "nivel1"
@@ -32,13 +37,14 @@ if [[ "$n" == "3 54" ]]; then
 for i in ${numeros2[@]}
 do
 	clear
-	echo $i
+	figlet -c $i
 	sleep 2
 	clear
 done
 read -p "> " n
 if [[ "$n" == "56 78 7" ]]; then
-	echo "pasaste al nivel 3"
+	echo -e "${BLUE}pasaste al nivel 3"
+	sleep 2
 	else
 		echo "Lo sentimos has perdido"
 		echo "nivel2"
@@ -47,13 +53,14 @@ if [[ "$n" == "56 78 7" ]]; then
 for i in ${numeros3[@]}
 do
 	clear
-	echo $i
+	figlet -c $i
 	sleep 1
 	clear
 done
 read -p "> " n
 if [[ "$n" == "65 8 5 65" ]]; then
-	echo "pasaste al nivel 4"
+	echo -e "${RED}pasaste al nivel 4"
+	sleep 2
 	else
 		echo "Lo sentimos has perdido"
 		echo "nivel3"
@@ -62,13 +69,14 @@ if [[ "$n" == "65 8 5 65" ]]; then
 for i in ${numeros4[@]}
 do
 	clear
-	echo $i
+	figlet -c $i
 	sleep 1
 	clear
 done
 read -p "> " n
 if [[ "$n" == "43 98 90 32 54" ]]; then
-	echo "pasaste al nivel 5"
+	echo -e "${GREEN}pasaste al nivel 5"
+	sleep 2
 	else
 		echo "Lo sentimos has perdido"
 		echo "nivel4"
@@ -77,14 +85,15 @@ if [[ "$n" == "43 98 90 32 54" ]]; then
 for i in ${numeros5[@]}
 do
 	clear
-	echo $i
+	figlet -c $i
 	sleep 1
 	clear
 done
 read -p "> " n
 if [[ "$n" == "76 56 98 34 90 63" ]]; then
-	echo "Felicidades has ganado el juego"
-	read -p "********************" fin
+	echo -e "${BLUE}*********************************************************************************************"
+	figlet -c  "Felicidades has ganado el juego"
+	read -p "               ****************************************************" fin
 	else
 		echo "Lo sentimos has perdido"
 		echo "nivel5"
